84d5cdc0dfcca29407a1a50b8b153dec3aa95936
Y. Yan <dev@ybank.net>
2016-07-24 03:56:16 -0700

    CLI-162-6a prev

--- src/test/org/apache/commons/cli/bug/BugCLI162Test.java
+++ src/test/org/apache/commons/cli/bug/BugCLI162Test.java
@@ -261,7 +261,7 @@ public class BugCLI162Test extends TestCase {
         assertEquals( "Long arguments did not split as expected", expected, sw.toString() );
     }
 
-    public void testLongLineChunkingIndentIgnored() throws ParseException, IOException {
+    public void testLongLineChunkingIndentIgnored_old() throws ParseException, IOException {
         Options options = new Options();
         options.addOption("x", "extralongarg", false, "This description is Long." );
         HelpFormatter formatter = new HelpFormatter();
@@ -295,6 +295,25 @@ public class BugCLI162Test extends TestCase {
                           "                     n\n" +
                           "                     g\n" +
                           "                     .\n" +
+                          "Footer\n";
+        assertEquals( "Long arguments did not split as expected", expected, sw.toString() );
+    }
+
+    public void testLongLineChunkingIndentIgnored() throws ParseException, IOException {
+        Options options = new Options();
+        options.addOption("x", "extralongarg", false, "This description is Long." );
+        HelpFormatter formatter = new HelpFormatter();
+        StringWriter sw = new StringWriter();
+        formatter.printHelp(new PrintWriter(sw), 22, this.getClass().getName(), "Header", options, 0, 5, "Footer");
+        System.err.println(sw.toString());
+        String expected = "usage:\n" +
+                          "       org.apache.comm\n" +
+                          "       ons.cli.bug.Bug\n" +
+                          "       CLI162Test\n" +
+                          "Header\n" +
+                          "-x,--extralongarg\n" +
+                          " This description is\n" +
+                          " Long.\n" +
                           "Footer\n";
         assertEquals( "Long arguments did not split as expected", expected, sw.toString() );
     }
