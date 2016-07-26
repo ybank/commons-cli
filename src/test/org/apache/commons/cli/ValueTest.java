1114a4275c55ed768158539bbd93874d55e1c591
Y. Yan <dev@ybank.net>
2016-07-26 04:35:16 -0700

    CLI-201-1a prev

--- src/test/org/apache/commons/cli/ValueTest.java
+++ src/test/org/apache/commons/cli/ValueTest.java
@@ -189,7 +189,7 @@ public class ValueTest extends TestCase
         assertTrue( !cmd.hasOption("fake") );
     }
 
-    public void testPropertyOptionFlags() throws Exception
+    public void testPropertyOptionFlags_old() throws Exception
     {
         Properties properties = new Properties();
         properties.setProperty( "a", "true" );
@@ -234,6 +234,53 @@ public class ValueTest extends TestCase
         assertTrue( !cmd.hasOption("a") );
         assertTrue( !cmd.hasOption("c") );
         assertTrue( !cmd.hasOption("e") );
+    } 
+
+    public void testPropertyOptionFlags() throws Exception
+    {
+        Properties properties = new Properties();
+        properties.setProperty( "a", "true" );
+        properties.setProperty( "c", "yes" );
+        properties.setProperty( "e", "1" );
+
+        Parser parser = new PosixParser();
+
+        CommandLine cmd = parser.parse(opts, null, properties);
+        assertTrue( cmd.hasOption("a") );
+        assertTrue( cmd.hasOption("c") );
+        assertTrue( cmd.hasOption("e") );
+
+
+        properties = new Properties();
+        properties.setProperty( "a", "false" );
+        properties.setProperty( "c", "no" );
+        properties.setProperty( "e", "0" );
+
+        cmd = parser.parse(opts, null, properties);
+        assertTrue( !cmd.hasOption("a") );
+        assertTrue( !cmd.hasOption("c") );
+        assertTrue( cmd.hasOption("e") ); // this option accepts as argument
+
+
+        properties = new Properties();
+        properties.setProperty( "a", "TRUE" );
+        properties.setProperty( "c", "nO" );
+        properties.setProperty( "e", "TrUe" );
+
+        cmd = parser.parse(opts, null, properties);
+        assertTrue( cmd.hasOption("a") );
+        assertTrue( !cmd.hasOption("c") );
+        assertTrue( cmd.hasOption("e") );
+
+        
+        properties = new Properties();
+        properties.setProperty( "a", "just a string" );
+        properties.setProperty( "e", "" );
+
+        cmd = parser.parse(opts, null, properties);
+        assertTrue( !cmd.hasOption("a") );
+        assertTrue( !cmd.hasOption("c") );
+        assertTrue( cmd.hasOption("e") );
     } 
 
     public void testPropertyOptionMultipleValues() throws Exception
