89ec268dbe9c798ec066e0f9fc4d7d53578ca5c0
Y. Yan <dev@ybank.net>
2016-07-26 04:42:41 -0700

    CLI-185-1a prev

--- src/test/org/apache/commons/cli/UtilTest.java
+++ src/test/org/apache/commons/cli/UtilTest.java
@@ -32,8 +32,17 @@ public class UtilTest extends TestCase
         assertNull(Util.stripLeadingHyphens(null));
     }
 
+    public void testStripLeadingAndTrailingQuotes_old()
+    {
+        assertEquals("foo", Util.stripLeadingAndTrailingQuotes("\"foo\""));
+    }
+
     public void testStripLeadingAndTrailingQuotes()
     {
         assertEquals("foo", Util.stripLeadingAndTrailingQuotes("\"foo\""));
+        assertEquals("foo \"bar\"", Util.stripLeadingAndTrailingQuotes("foo \"bar\""));
+        assertEquals("\"foo\" bar", Util.stripLeadingAndTrailingQuotes("\"foo\" bar"));
+        assertEquals("\"foo\" and \"bar\"", Util.stripLeadingAndTrailingQuotes("\"foo\" and \"bar\""));
+        assertEquals("\"", Util.stripLeadingAndTrailingQuotes("\""));
     }
 }
